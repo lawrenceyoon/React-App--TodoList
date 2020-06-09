725a2aa408f8e1153a384b7138f4a6301716c614
Lawrence <yoonlawrence@yahoo.com>
2020-06-09 16:19:42 -0700

    Checking sass

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+// dependencies
+import React from 'react';
+// local files
+import './TodoList.scss';
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div className="TodoList">
+        Hello from TodoList!
+      </div>
+    );
+  };
+};
+
+export default TodoList;
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,15 @@
+// dependencies
 import React from 'react';
+// local files
+import './App.scss';
+import TodoList from '../TodoList';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         Hello from App Component!
+        <TodoList />
       </div>
     );
   };
